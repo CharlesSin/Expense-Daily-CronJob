835b4fc5c4e94341839cd350e921d894b4959deb
charles_sin <charles_sin@superbtech.asia>
2024-03-21 11:14:31 +0800

    1. add gke resource => cronjob.yaml 2. add build.sh => for docker build image & push image to docker hub

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#部署環境 [讀取第一個參數]
+BUILDENV=$1
+
+#版本號碼 [讀取第二個參數]
+VERSION=$2
+
+if [ -z "$BUILDENV" ]; then
+    echo "用法: sh build.sh [Arg1] [Arg2]"
+    echo "Arg1: Build版環境(local/dev/demo/prod)"
+    echo "Arg2: 版本號碼"
+    exit 0
+fi
+
+if [ -z "$VERSION" ]; then
+    echo "\n沒有指定版本號碼"
+    exit 0
+fi
+
+echo "\n建立image檔案..."
+docker system prune -f
+docker build -t daily-backup-cronjob .
+
+if [ "$BUILDENV" != "local" ]; then
+    # 將 Image 加上 tag
+    echo "\n标记本地镜像"
+    docker tag daily-backup-cronjob eugeneyiew128/daily-backup-cronjob:"$BUILDENV"_"$VERSION"
+
+    # docker login docker.io
+    docker login -u eugeneyiew128 -p 93112808Yiew docker.io
+
+    # 將 Image 推到 artifacts registry 上
+    echo "\n上傳至 Docker Hub"
+    docker push eugeneyiew128/daily-backup-cronjob:"$BUILDENV"_"$VERSION"
+
+    # 部署
+    echo "\n部署到 GKE 上"
+    kubectl apply -f gke-resource/cronjob.yaml
+
+    # 更新
+    echo "\n更新 Docker Image"
+    kubectl set image cronjob/daily-backup-cronjob cronjob-test=eugeneyiew128/daily-backup-cronjob:"$BUILDENV"_"$VERSION"
+
+    echo "\nAll Done!!"
+fi
